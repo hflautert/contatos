a18377769ca4986ef25a6da123b3bea48c7c8d2a
Henrique Fell Lautert <hflautert@gmail.com>
2015-02-02 17:07:47 -0500

    Edita todos campos direto na linha

--- neoc/static/js/controllers.js
+++ neoc/static/js/controllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var contatosApp = angular.module('contatosApp', []);
+var contatosApp = angular.module('contatosApp',['xeditable']);
 
 contatosApp.controller('ContatoListCtrl', function ($scope,$http){
         console.log("Entrou no List...");
@@ -10,12 +10,63 @@ contatosApp.controller('ContatoListCtrl', function ($scope,$http){
 	});
 });
 
-/* Funcao para adicionar/editar contato - EM CONSTRUÇÃO
-No momento está adicionando contatos com os comentários abaixo. */
-contatosApp.controller('ContatoEditCtrl', function ($scope,$http){
+/* contatosApp.controller('ContatoEditCtrl', function ($scope,$http){
         console.log("Entrou no Edit...");
-        /* var res = $http.post('apicontatos/',{nome:'Via http apii', email:'h@h.com', fone:'3333', mfone:'2222'});
-        res.success(function(data) {
-                console.log("Data:",data);
-        }); */
+	// 2 funcoes OK
+	// post adcionar novo
+	// put edita
+	//var res = $http.post('apicontatos/',{nome:'Via http apii', email:'h@h.com', fone:'3333', mfone:'2222'});
+        //var res = $http.put('apicontatos/1/',{nome:'Henrique Fell Lautert', email:'h@h.com', fone:'3333', mfone:'2222'});
+        //res.success(function(data) {
+        //        console.log("Data:",data);
+        //});
+}); */
+
+contatosApp.controller('ContatoEditNome', function($scope, $http) {
+        $scope.updateUser = function(data) {
+		return $http.put( 'apicontatos/'+$scope.contato.id+'/',{
+			nome: data, 
+			fone: $scope.contato.fone,
+			mfone: $scope.contato.mfone,
+			email: $scope.contato.email 
+			});
+	};
+});
+
+contatosApp.controller('ContatoEditFone', function($scope, $http) {
+        $scope.updateUser = function(data) {
+		return $http.put( 'apicontatos/'+$scope.contato.id+'/',{
+			nome: $scope.contato.nome, 
+			fone: data,
+			mfone: $scope.contato.mfone,
+			email: $scope.contato.email 
+			});
+	};
+});
+
+contatosApp.controller('ContatoEditMfone', function($scope, $http) {
+        $scope.updateUser = function(data) {
+		return $http.put( 'apicontatos/'+$scope.contato.id+'/',{
+			nome: $scope.contato.nome, 
+			fone: $scope.contato.fone,
+			mfone: data,
+			email: $scope.contato.email 
+			});
+	};
+});
+
+contatosApp.controller('ContatoEditEmail', function($scope, $http) {
+        $scope.updateUser = function(data) {
+		return $http.put( 'apicontatos/'+$scope.contato.id+'/',{
+			nome: $scope.contato.nome, 
+			fone: $scope.contato.fone,
+			mfone: $scope.contato.mfone,
+			email: data 
+			});
+	};
+});
+
+// Altera thema do Xeditable
+contatosApp.run(function(editableOptions) {
+  editableOptions.theme = 'bs3';
 });
